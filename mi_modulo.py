643b1f9a005a37a4b38c9c6bf17924466092e347
danoc93 <dortiz93@gmail.com>
2022-07-01 22:33:01 +0100

    Creando nueva funcion

--- mi_modulo.py
+++ mi_modulo.py
@@ -1,4 +1,9 @@
 def mi_funcion(numero1: int, numero2: int) -> str:
+    return f"Los numeros son {numero1} {numero2}"
+
+def mi_funcion2(numero1: int, numero2: int) -> str:
     return f"{numero1} {numero2}"
 
 print(mi_funcion(5, 6))
+
+print(mi_funcion2(5, 6))
